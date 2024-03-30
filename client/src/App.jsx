a2da9d15af06cd92e63eca34c0d508d92a959d0f
Shawn Meister <shawnmeister@Elizabeths-MacBook-Air.local>
2024-03-30 12:57:21 -0300

    completed Apollo Client & Provider config in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,52 @@
-// import React from 'react';
-// import SearchBooks from './pages/SearchBooks';
-// import SavedBooks from './pages/SavedBooks';
+/**
+ * Apollo Client setup and provider for GraphQL requests
+ *
+ * Imports necessary Apollo Client dependencies and creates an Apollo Client instance configured to make requests to the GraphQL API at '/graphql'.
+ * Includes an auth middleware that adds the JWT token to requests if present.
+ * Renders the App component wrapped in an ApolloProvider to make the client available to connected components.
+ */
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import {
   ApolloClient,
+  HttpLink,
+  ApolloLink,
   InMemoryCache,
-  ApolloProvider
+  concat,
+  ApolloProvider,
 } from '@apollo/client';
-// import { setContext } from "@apollo/client/link/context";
 
+// create an http link endpoint
+const httpLink = new HttpLink({
+  uri: '/graphql',
+});
+
+// create an auth middleware to add the token to the headers
+const authMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('id_token') || null,
+    },
+  }));
+
+  return forward(operation);
+});
+
+// execute the auth middleware before GraphQL requests
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
   cache: new InMemoryCache(),
+  link: concat(authMiddleware, httpLink),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
     </ApolloProvider>
   );
 }
